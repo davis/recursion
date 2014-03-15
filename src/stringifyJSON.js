be81af032eaa33ba4dec4f9225f5d1edbc60590b
Davis <dkim1@illumina.com>
2014-03-14 17:50:55 -0700

    started on stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,34 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+	var result;
+	if (obj === null) {
+		result = "null";
+	} else if(typeof(obj) === "number" || typeof(obj) === "boolean") {
+		result = obj.toString();
+	} else if(typeof(obj) === "string") {
+		result = "\"" + obj + "\""; 
+	} else if(Array.isArray(obj)) {
+		result = "[" + obj.toString() + "]";
+	}
+	return result;
 };
+
+
+
+
+
+
+
+/* 	var string = "";
+  		_.each(obj, function(value, key) {
+  			string += '"' + key.toString() + '"';
+  			string += ':';
+  			string += value.toString();
+  			string += ',';
+  		});
+  		
+  		if (typeof(obj) === 'object') {
+  			string.slice(0,string.length-1)
+  		}
+*/
