2fcdfb9a4cf5272496fe9642ce5355160684188c
Davis <dkim1@illumina.com>
2014-03-17 17:48:34 -0700

    started getElements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,7 +3,15 @@
 //   return document.getElementsByClassName(className);
 // };
 
-// But in stead we're going to implement it from scratch:
+// But instead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
-  // your code here
+	var results = [];
+	for(i = 0; i < document.body.childNodes.length; i++) {
+			if(document.body.childNodes[i].classList)
+				if(document.body.childNodes[i].classList.contains(className)) {
+				console.log(document.body.childNodes[i] + i);
+				results.push(document.body.childNodes[i]);
+		}
+	}
+	return results;
 };
